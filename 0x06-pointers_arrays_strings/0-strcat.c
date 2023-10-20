be135b43d47282c9d61de0a4f6b6a447804e4a1d
Hamza <semlalihamzauniv@gmail.com>
2023-10-20 14:16:11 -0700

    wow

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,57 +1,26 @@
-#include "main.h"
-
+#include "holberton.h"
 /**
- * _strcat - concatenates two strings
- * @dest: string to append to
- * @src: string to add
- *
- * Return: a pointer to the resulting string
+ * _strcat - concatenates 2 strings.
+ * @dest: string with concatenation
+ * @src: string to be concatenated
+ * Return: Always 0.
  */
 char *_strcat(char *dest, char *src)
 {
-	int i, j;
+	int lengthD, lengthS;
 
-	i = 0;
-	j = 0;
+	lengthD = 0;
+	lengthS = 0;
 
-	while (dest[i] != '\0')
-		i++;
+	while (*(dest + lengthD) != '\0')
+		lengthD++;
 
-	while (src[j] != '\0')
+	while (*(src + lengthS) != '\0' && lengthD < 97)
 	{
-		dest[i] = src[j];
-		j++;
-		i++;
+		*(dest + lengthD) = *(src + lengthS);
+		lengthD++;
+		lengthS++;
 	}
-
-	dest[i] = '\0';
-
-	return (dest);
-}
-#include "main.h"
-/**
- * _strcat - concatenates two strings,
- * @dest: destination.
- * @src: source.
- * Return: the pointer to dest.
- */
-char *_strcat(char *dest, char *src)
-{
-	int s1 = 0;
-	int s2 = 0;
-
-	while (*(dest + s1) != '\0')
-	{
-		s1++;
-	}
-
-	while (s2 >= 0)
-	{
-		*(dest + s1) = *(src + s2);
-		if (*(src + s2) == '\0')
-			break;
-		s1++;
-		s2++;
-	}
+	*(dest + lengthD) = '\0';
 	return (dest);
 }
