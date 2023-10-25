eba42db9f7c78b7b6a286f5c3f13a9ad7fd7e375
listenseedestroy <semlalihamzauniv@gmail.com>
2023-10-25 21:31:41 +0100

    betty

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,5 +1,13 @@
 #include "main.h"
-
+/**
+ * wildcmp - compares two strings and returns 1
+ * if the strings can be considered identical,
+ * otherwise return 0.
+ * @s1: the normal string
+ * @s2: the special string containing "*"
+ *
+ * Return: 1 if identical, else 0
+ */
 int wildcmp(char *s1, char *s2)
 {
 	if (*s2 == '\0' && *s1 == '\0')
