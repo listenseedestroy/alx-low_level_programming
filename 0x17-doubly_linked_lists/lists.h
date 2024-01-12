c68e1e297cc7290ef8527e70d8621ec3c5cb693a
listenseedestroy <semlalihamzauniv@gmail.com>
2024-01-12 23:06:29 +0100

    fixed lists.h

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -4,15 +4,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/**
- * struct dlistint_s - doubly linked list
- * @n: integer
- * @prev: points to the previous node
- * @next: points to the next node
- *
- * Description: doubly linked list node structure
- * for Holberton project
- */
 typedef struct dlistint_s
 {
 	int n;
